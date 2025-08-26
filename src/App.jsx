9b874cff5f1cff835d1d2b77b71ae53aa5a00368
Anand <anandhoni183@gmail.com>
2025-08-26 11:11:51 +0530

    Setting up. Iniial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white text-4xl font-bold">
+      PORTFOLIO
+    </div>
+  );
+}
+export default App;
